27e0a1a9e254a3a0dfc3e22eba4d5608990d2b7e
vuong <vuongphan139.it@gmail.com>
2022-08-10 16:39:44 +0700

    Finishes modeling the product model along with user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   before_create :generate_authentication_token!
 
+  has_many :products, dependent: :destroy
+
   validates :auth_token, uniqueness: true
 
   def generate_authentication_token!
