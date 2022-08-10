6f8b76ec309b4874300d1de2598f59e0f1ba46b5
vuong <vuongphan139.it@gmail.com>
2022-08-11 00:19:20 +0700

    Embeds the products_ids into the user serialiser and fetches the correct products from the index action endpoint

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :created_at, :updated_at, :auth_token
+
+  has_many :products, key: :product_ids do
+    object.product_ids
+  end
 end
